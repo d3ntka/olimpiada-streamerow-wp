22a9eeb8348a2d066b5f2d9d61f0e8f4d7a9d627
d3ntka <77463886+d3ntka@users.noreply.github.com>
2022-02-14 20:40:49 +0100

    init

---
+++ wp-content/themes/olimpiada-streamerow/main.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Main
+ * Description: Front-page
+ *
+ */
+
+get_header();
+
+the_post();
+?>
+
+<div class="section1">
+	<div class="title">
+		<div class="olives">
+			<i class="olive-left"></i> 
+			<span>
+				Igrzyska
+			</span>
+			<i class="olive-right"></i> 
+		</div>
+		rozpoczęte
+	</div>
+	<div class="people">
+
+	</div>
+</div>
+
+<div class="section2">
+	<div class="title">
+		<div class="olives">
+			<i class="olive-left"></i> 
+			<h2>
+				Znamy już dwie dyscypliny
+			</h2>
+			<i class="olive-right"></i> 
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
